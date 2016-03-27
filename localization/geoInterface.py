a4b989c9dc63adb846ce7f066d1ca92790a04373
Elvio Toccalino <me@etoccalino.com>
2016-03-27 11:45:52 -0300

    first attempt at PEP8 compliance

--- localization/geoInterface.py
+++ localization/geoInterface.py
@@ -15,29 +15,27 @@
 # You should have received a copy of the GNU General Public License along with
 # Localization.  If not, see <http://www.gnu.org/licenses/>.
 
-""" Anchor and target class"""
-
 
 class Anchor:
-	def __init__(self,ID,loc):
-		self.loc=loc
-		self.ID=str(ID)
+    def __init__(self, ID, loc):
+        self.loc = loc
+        self.ID = str(ID)
 
-	def __str__(self):
-		return 'Anchor '+self.ID+' @ '+self.loc.__str__()
+    def __str__(self):
+        return 'Anchor '+self.ID+' @ '+self.loc.__str__()
 
 
 class Target:
-	def __init__(self,ID):
-		self.loc=None
-		self.ID=str(ID)
-		self.measures=[]
-	def __str__(self):
-		if self.loc is None:
-			return 'Target '+self.ID
-		else:
-			return 'Target '+self.ID+' @ Real Location:'+self.loc.__str__()
+    def __init__(self, ID):
+        self.loc = None
+        self.ID = str(ID)
+        self.measures = []
 
-	def add_measure(self,a,d):
-		self.measures.append((a,d))
+    def __str__(self):
+        if self.loc is None:
+            return 'Target '+self.ID
+        else:
+            return 'Target '+self.ID+' @ Real Location:'+self.loc.__str__()
 
+    def add_measure(self, a, d):
+        self.measures.append((a, d))
