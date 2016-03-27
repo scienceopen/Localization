49b3fbe46e58ac669ecfabfb72c1eaa3c1723ff3
Elvio Toccalino <me@etoccalino.com>
2016-03-27 10:16:08 -0300

    added LGPL license

--- localization/find_centroid.py
+++ localization/find_centroid.py
@@ -1,3 +1,20 @@
+# Copyright 2016 Elvio Toccalino, Kamal Shadi
+
+# This file is part of the Localization package.
+#
+# Localization is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Localization is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Localization.  If not, see <http://www.gnu.org/licenses/>.
+
 import shapely as shx
 from shapely.geometry import Polygon,Point,MultiPolygon
 import numpy as num
