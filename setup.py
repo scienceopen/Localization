e5a061ee45ecd16ebb00a26d22e83df6a5cb1088
Elvio Toccalino <me@etoccalino.com>
2016-03-27 10:19:22 -0300

    cleaned up unused files

--- setup.py
+++ setup.py
@@ -20,12 +20,15 @@ from distutils.core import setup
 setup(
     name='Localization',
     version='0.1.4',
-    author='Kamal Shadi',
-    author_email='kamal.shadi85@gmail.com',
+
+    author='Elvio Toccalino',
+    author_email='me@etoccalino.com',
+
     packages=['localization', 'localization.test'],
-    scripts=['bin/sample.py'],
+
     url='https://github.com/kamalshadi/Localization',
     license='LICENSE.txt',
+
     description='Multilateration and triangulation.',
     long_description=open('README.txt').read(),
 )
